189fd55532ed8e7564de97ff1a3a76086e1d73e0
= <wesley.rebelo@sap.com>
2015-11-03 17:56:45 -0800

    todas as paginas adicionadas ao menu

--- app/config.js
+++ app/config.js
@@ -18,6 +18,96 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 				}
 			}
 		})
+		.state('historia', {
+			url: "/historia",
+			views: {
+				'topNavBar': {
+					templateUrl: 'app/pages/topNavBar.html',
+					controller: 'TopNavBarController'
+				},
+				'main': {
+					templateUrl: 'app/pages/historia.html'
+					//controller: 'PersonListController'
+				},
+				'menu': {
+					templateUrl: 'app/pages/menu.html',
+					controller: 'MenuController'
+
+				}
+			}
+		})
+		.state('repertorio', {
+			url: "/repertorio",
+			views: {
+				'topNavBar': {
+					templateUrl: 'app/pages/topNavBar.html',
+					controller: 'TopNavBarController'
+				},
+				'main': {
+					templateUrl: 'app/pages/repertorio.html'
+					//controller: 'PersonListController'
+				},
+				'menu': {
+					templateUrl: 'app/pages/menu.html',
+					controller: 'MenuController'
+
+				}
+			}
+		})
+		.state('fotos', {
+			url: "/fotos",
+			views: {
+				'topNavBar': {
+					templateUrl: 'app/pages/topNavBar.html',
+					controller: 'TopNavBarController'
+				},
+				'main': {
+					templateUrl: 'app/pages/fotos.html'
+					//controller: 'PersonListController'
+				},
+				'menu': {
+					templateUrl: 'app/pages/menu.html',
+					controller: 'MenuController'
+
+				}
+			}
+		})
+		.state('videos', {
+			url: "/videos",
+			views: {
+				'topNavBar': {
+					templateUrl: 'app/pages/topNavBar.html',
+					controller: 'TopNavBarController'
+				},
+				'main': {
+					templateUrl: 'app/pages/videos.html'
+					//controller: 'PersonListController'
+				},
+				'menu': {
+					templateUrl: 'app/pages/menu.html',
+					controller: 'MenuController'
+
+				}
+			}
+		})
+		.state('cds', {
+			url: "/cds",
+			views: {
+				'topNavBar': {
+					templateUrl: 'app/pages/topNavBar.html',
+					controller: 'TopNavBarController'
+				},
+				'main': {
+					templateUrl: 'app/pages/cds.html'
+					//controller: 'PersonListController'
+				},
+				'menu': {
+					templateUrl: 'app/pages/menu.html',
+					controller: 'MenuController'
+
+				}
+			}
+		})
 		.state('contato', {
 			url: "/contato",
 			views: {
